1b0650f6f1e544754aa5906521304ea358c613e7
gixi <gixipixel@gmail.com>
2024-02-29 13:25:27 -0300

    cambios para deploy en shuttle

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,16 @@
 extern crate rocket;
 use rocket_dyn_templates::{context, Template};
 
+#[shuttle_runtime::main]
+async fn main() -> shuttle_rocket::ShuttleRocket {
+    let rocket: rocket::Rocket<rocket::Build> = rocket::build()
+        .mount("/", routes![index])
+        .attach(Template::fairing());
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
-    .attach(Template::fairing())
-
+    Ok(rocket.into())
 }
 
 #[get("/")]
 fn index() -> Template {
     Template::render("index", context! {title: "lym"})
 }
-
-/* abrir apps o archivos lym open [app] */
-
